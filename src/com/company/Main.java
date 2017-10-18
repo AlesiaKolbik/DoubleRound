a855b4e7ca3683b44fae3d9c09b6f97ccb800282
alesya.kolbik <moyaalesia@mail.ru>
2017-10-18 13:19:10 +0300

    треугольник из единиц с помощью for

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1;i<=10 ; i++) {
+            int j;
+            for ( j = 1; j<=i+1 ; j++) {
+                System.out.printf("1 ");
+
+            }
+            System.out.println(" ");
+        }
+    }
+}
