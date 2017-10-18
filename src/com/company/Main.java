da978694c8da858f38b19eaa4b018eb720a2849c
alesya.kolbik <moyaalesia@mail.ru>
2017-10-18 13:38:31 +0300

    обратный треугольник

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1;i<=10 ; i++) {
-            int j;
-            for ( j = 1; j<=i+1 ; j++) {
-                System.out.printf("1 ");
 
+        for(int i=1;i<=10;i++)
+        {
+            for(int j=1; j<=10-i; j++)
+            {
+                System.out.print(i+" ");
             }
             System.out.println(" ");
         }
